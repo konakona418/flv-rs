049aa46a2348f7ed855c1c56793d392aa2630da6
Creeper2333 <1642253282@qq.com>
2024-10-25 13:02:01 +0800

    feat: u32 bit manipulation.

--- src/io/bit.rs
+++ src/io/bit.rs
@@ -109,3 +109,64 @@ impl U16BitIO {
         }
     }
 }
+
+pub struct U32BitIO {
+    data: [u8; 4],
+    endian: U16ParserEndian
+}
+
+impl U32BitIO {
+    #[inline]
+    pub fn new(data: u32, endian: U16ParserEndian) -> U32BitIO {
+        let data = match endian {
+            U16ParserEndian::LittleEndian => {
+                data.to_le_bytes()
+            }
+            U16ParserEndian::BigEndian => {
+                data.to_be_bytes()
+            }
+        };
+        Self {
+            data,
+            endian
+        }
+    }
+
+    #[inline]
+    pub fn read_at(&mut self, bit_offset: usize) -> bool {
+        let byte_offset = bit_offset / 8;
+        let bit_offset = bit_offset % 8;
+
+        let byte = self.data[byte_offset];
+        let mask = 1 << (7 - bit_offset);
+        byte & mask != 0
+    }
+
+    #[inline]
+    pub fn read_range(&mut self, start: usize, end: usize) -> u32 {
+        let mut result = 0;
+        for i in start..end + 1 {
+            result |= (self.read_at(i) as u32) << (end - i);
+        }
+        result
+    }
+
+    #[inline]
+    pub fn write_at(&mut self, bit_offset: usize, value: bool) {
+        let byte_offset = bit_offset / 8;
+        let bit_offset = bit_offset % 8;
+        let mask = 1 << (7 - bit_offset);
+        if value {
+            self.data[byte_offset] |= mask;
+        } else {
+            self.data[byte_offset] &= !mask;
+        }
+    }
+
+    #[inline]
+    pub fn write_range(&mut self, start: usize, end: usize, value: u32) {
+        for i in start..end + 1 {
+            self.write_at(i, (value & (1 << (end - i))) != 0);
+        }
+    }
+}
